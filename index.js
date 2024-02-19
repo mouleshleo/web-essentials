ef9d2aba63c9a7948c47e22b33fa7956ace408b0
Your Name <you@example.com>
2024-02-19 16:11:09 +0530

    changed to MaP

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-AutoType("words", "Fuel your ambition with Edx", 100);
+AutoType("words", "Fuel your ambition with MaP", 100);
 
 function project() {
     window.open("https://github.com/mouleshleo/web-essentials");
