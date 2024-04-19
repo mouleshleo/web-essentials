a5dc539295073e44b77b3e0f5004b4b4d38c4a37
mouleshleo <mouleshleo06@gmail.com>
2024-04-19 23:03:26 +0530

    loader added

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ new kursor({
 
 });
 
+document.addEventListener("DOMContentLoaded",() => {
+ setTimeout(() => {
+    hideLoader();
+    showContent();
+},3000);
+});
+
+function hideLoader() {
+    const loader = document.getElementById("loader");
+    loader.style.display = 'none';
+}
+
+function showContent() {
+    const content = document.getElementById("content");
+    content.style.display = 'block';
+}
