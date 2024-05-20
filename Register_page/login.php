14db2c0b8d35e524af3a86a528bdb1b033bb4598
mouleshleo <mouleshleo06@gmail.com>
2024-05-20 17:11:20 +0530

    login updated

--- Register_page/login.php
+++ Register_page/login.php
@@ -10,8 +10,7 @@
 
         $conn = mysqli_connect("localhost","root","");
         if($conn === false){
-             die("Error : Could not connect. " 
-             . mysqli_connect_error());
+             die("Error : Could not connect. " . mysqli_connect_error());
         }
        $createdb = "CREATE DATABASE IF NOT EXISTS map_register;";
        $select = "USE map_register;";
@@ -23,17 +22,23 @@
         phone INT,
         password VARCHAR(30) NOT NULL
         );";
+        
         if (!mysqli_query($conn, $createdb)) {
           echo "error: ". mysqli_error($conn);
         }
+    
         mysqli_query($conn, $select);
-        mysqli_query($conn, $createtable);
+
+        if (!mysqli_query($conn, $createtable)) {
+          echo "Error creating table: " . mysqli_error($conn);
+        }
        $password=md5($password);
        $checkemail="SELECT * FROM register where email='$email';";
        $result=mysqli_query($conn,$checkemail);
        
-       if($result->num_rows > 0){
+       if(mysqli_num_rows($result) > 0){
         echo "Email address already exists !!";
+        exit();
        }
        else{
         $sql= "INSERT INTO register (fullname, email, username, phone, password) VALUES('$name','$email','$username','$phone','$password')";
