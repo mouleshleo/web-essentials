661014e3a2f6118bec7961516b168b9b8a4ffc0a
mouleshleo <mouleshleo06@gmail.com>
2024-05-20 16:35:58 +0530

    database corrected

--- Login_page/login.php
+++ Login_page/login.php
@@ -3,11 +3,21 @@
 <head>
     <title>Login</title>
 </head>
-<body><center>
+<body>
     <?php 
        include 'connect.php';
        extract($_POST);    
        $password=md5($password);
+       $createdb = "CREATE DATABASE IF NOT EXISTS map_register;";
+       $select = "USE DATABASE map_register;";
+       $createtable = "CREATE TABLE IF NOT EXISTS register {
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        email"
+       if (mysqli_query($conn, $createdb)) {
+            echo "successfull<br>";
+       } else {
+        echo "error : ". mysqli_error($conn);
+       }
         $sql= "SELECT * FROM register WHERE email='$email' and password='$password' ";
         $result=$conn->query($sql);
         if($result->num_rows>0){
@@ -23,8 +33,5 @@
         }
   ?>
 
-
-
-</center>
 </body>
 </html>
--- Register_page/login.php
+++ Register_page/login.php
@@ -8,32 +8,42 @@
 
        extract($_POST);    
 
-        $conn = mysqli_connect("localhost","root","","Map_Register");
+        $conn = mysqli_connect("localhost","root","");
         if($conn === false){
              die("Error : Could not connect. " 
              . mysqli_connect_error());
         }
-       if($password===$passwordd){ 
-        
+       $createdb = "CREATE DATABASE IF NOT EXISTS map_register;";
+       $select = "USE map_register;";
+       $createtable = "CREATE TABLE IF NOT EXISTS register (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        fullname VARCHAR(30) NOT NULL,
+        email VARCHAR(60),
+        username VARCHAR(30) NOT NULL,
+        phone INT,
+        password VARCHAR(30) NOT NULL
+        );";
+        if (!mysqli_query($conn, $createdb)) {
+          echo "error: ". mysqli_error($conn);
+        }
+        mysqli_query($conn, $select);
+        mysqli_query($conn, $createtable);
        $password=md5($password);
-       $checkemail="SELECT * FROM register where email='$email' ";
+       $checkemail="SELECT * FROM register where email='$email';";
        $result=mysqli_query($conn,$checkemail);
-       if($result->num_rows>0){
+       
+       if($result->num_rows > 0){
         echo "Email address already exists !!";
        }
        else{
-        $sql= "INSERT INTO register VALUES('$name','$email','$phone','$username','$password')";
-       }}
-       else{
-         echo "Both passwords are Different reenter your password";
+        $sql= "INSERT INTO register (fullname, email, username, phone, password) VALUES('$name','$email','$username','$phone','$password')";
        }
 
         if(mysqli_query($conn,$sql)){
              header("location: ../index.html");
         }
         else{
-            echo "sorry error $sql"
-            .mysqli_error($conn);
+            echo "sorry error $sql".mysqli_error($conn);
         }
   mysqli_close($conn);
   ?>
